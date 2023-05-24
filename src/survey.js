63da7764e28112d71b35f5d6419330670f744b07
Nic <nmweber@uw.edu>
2023-05-24 11:01:53 -0700

    add query and display results

--- src/survey.js
+++ src/survey.js
@@ -1,7 +1,16 @@
-import React from "react";
-import { Button, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Form, Table } from "react-bootstrap";
+import axios from 'axios';
 
 const Survey = () => {
+  const [username, setUsername] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  const fetchRepos = async () => {
+    const response = await axios.get(`https://api.github.com/users/${username}/repos`);
+    setRepos(response.data);
+  };
+
   return (
     <Form>
       <Form.Group>
@@ -13,6 +22,31 @@ const Survey = () => {
         <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
+
+      <Form.Group>
+        <Form.Label>Github Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter Github username" value={username} onChange={e => setUsername(e.target.value)} />
+        <Button variant="primary" onClick={fetchRepos}>Search for my repositories</Button>
+      </Form.Group>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Repo Name</th>
+            <th>Repo URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {repos.map((repo, index) => (
+            <tr key={repo.id}>
+              <td>{index + 1}</td>
+              <td>{repo.name}</td>
+              <td><a href={repo.html_url} target="_blank" rel="noreferrer">{repo.html_url}</a></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
 
       <Button variant="primary" type="submit">
         Submit

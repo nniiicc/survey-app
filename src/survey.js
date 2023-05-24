5d644883f8b017085501a818cf17321967a55b10
Nic <nmweber@uw.edu>
2023-05-24 11:14:37 -0700

    push select for table

--- src/survey.js
+++ src/survey.js
@@ -1,26 +1,49 @@
 import React, { useState } from "react";
 import { Button, Form, Table } from "react-bootstrap";
 import axios from 'axios';
+import { CSVLink } from "react-csv";
 
 const Survey = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [repos, setRepos] = useState([]);
+  const [selectedRepos, setSelectedRepos] = useState([]);
+  const [csvData, setCsvData] = useState([]);
 
   const fetchRepos = async () => {
     const response = await axios.get(`https://api.github.com/users/${username}/repos`);
     setRepos(response.data);
   };
 
+  const handleCheckboxChange = (repo) => {
+    if (selectedRepos.includes(repo)) {
+      setSelectedRepos(selectedRepos.filter((r) => r !== repo));
+    } else {
+      setSelectedRepos([...selectedRepos, repo]);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = selectedRepos.map((repo) => ({
+      Name: name,
+      Email: email,
+      Repository: repo,
+    }));
+    setCsvData(data);
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group>
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" />
+        <Form.Control type="text" placeholder="Enter name" value={name} onChange={e => setName(e.target.value)} />
       </Form.Group>
 
       <Form.Group>
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
       </Form.Group>
 
       <Form.Group>
@@ -35,6 +58,7 @@ const Survey = () => {
             <th>#</th>
             <th>Repo Name</th>
             <th>Repo URL</th>
+            <th>Select</th>
           </tr>
         </thead>
         <tbody>
@@ -43,6 +67,7 @@ const Survey = () => {
               <td>{index + 1}</td>
               <td>{repo.name}</td>
               <td><a href={repo.html_url} target="_blank" rel="noreferrer">{repo.html_url}</a></td>
+              <td><Form.Check type="checkbox" onChange={() => handleCheckboxChange(repo.name)} /></td>
             </tr>
           ))}
         </tbody>
@@ -51,6 +76,16 @@ const Survey = () => {
       <Button variant="primary" type="submit">
         Submit
       </Button>
+      {csvData.length > 0 && (
+        <CSVLink
+          data={csvData}
+          filename={"survey-data.csv"}
+          className="btn btn-primary"
+          target="_blank"
+        >
+          Download survey data
+        </CSVLink>
+      )}
     </Form>
   );
 };
